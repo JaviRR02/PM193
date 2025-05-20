00fbcee18f0566223fe1cf0c2e42c43573131136
Javier Rodríguez <112576867+JaviRR02@users.noreply.github.com>
2025-05-19 19:31:31 -0600

    Update ejercicio1.js

--- practica1/ejercicio1.js
+++ practica1/ejercicio1.js
@@ -6,4 +6,14 @@ nombre = "Ana Maria";
 
 var saludo = "Hola, " + nombre + ". Tienes " + edad + " años.";
 
+console.log(saludo);
 //muestra en consola en contenido de saludo
+
+var nombre = "Javier";
+const edad = "22";
+
+nombre = "Francisco Rodriguez";
+
+let saludo = "Hola, " + nombre + ". Tienes " + edad + " años.";
+
+console.log(saludo);
