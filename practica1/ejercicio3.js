3d972e5cc6d024413b0360168d56cdd82bafb4de
Javier Rodríguez <xavilk2.0@gmail.com>
2025-05-19 22:07:59 -0600

    Crear función saludoPersonalizado que retorna un saludo con nombre y edad

--- practica1/ejercicio3.js
+++ practica1/ejercicio3.js
@@ -1,2 +1,6 @@
-/*Crea una arrow function llamada saludoPersonalizado que reciba dos parametros: nombre y edad, retorne una cadena como la siguiente  */
+/*Crea una arrow function llamada saludoPersonalizado que reciba dos parametros: nombre y edad, 
+retorne una cadena como la siguiente  */
 //"Hola, me llamo Isay y tengo 37 años."
+
+const saludoPersonalizado = (nombre, edad) => "Hola, me llamo " + nombre + " y tengo " + edad + " años.";
+console.log(saludoPersonalizado("Javier", 22));
