7620a1060427e7b6fc658fb9f4c554782f441045
Javier Rodríguez <xavilk2.0@gmail.com>
2025-06-12 16:07:40 -0600

    añadimos componentes en app.js

--- my-app/App.js
+++ my-app/App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+const Texto = (props) => {
+  const{contenido}=props
+  return(
+    <Text>{contenido}</Text>
+  )
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Texto contenido='Hola'></Texto>
+      <Texto contenido='Mundo'></Texto>
+      <Texto contenido='ReactNative'></Texto>
+      <Button title='Presionar'></Button>
+      <StatusBar style='auto'/>
     </View>
   );
 }
--- practica2/ejercicioC.js
+++ practica2/ejercicioC.js
@@ -4,19 +4,19 @@ const personas = [
     { nombre: "Maria", edad: 28 }
 ];
 
-const luis = personas.find(persona => persona.nombre === "Luis");
+const luis = personas.find(personas => personas.nombre === "Luis");
 console.log(luis);
 // Salida: { nombre: 'Luis', edad: 35 }
 
-personas.forEach(persona => {
-    console.log(`El nombre de la persona es ${persona.nombre} y su edad es ${persona.edad}`);
+personas.forEach(personas => {
+    console.log(`El nombre de la persona es ${personas.nombre} y su edad es ${personas.edad}`);
 });
 
 // const sumaEdades = personas.reduce((acumulador, persona) => acumulador + persona.edad, 0);
 // console.log(`La suma de las edades es ${sumaEdades}`);
 // Salida: La suma de las edades es 85
 
-const edades = personas.map(persona => persona.edad);
+const edades = personas.map(personas => personas.edad);
 
 const sumaEdades = edades.reduce((acumulador, edad) => acumulador + edad);
 console.log(`La suma de las edades es ${sumaEdades}`);
